c0bc77ecba4fbad3b0be2a82499c6cdf4c2b7761
Oliver Charles <ollie@ocharles.org.uk>
2015-01-16 20:09:54 +0000

    Regenerate nix expressions

--- default.nix
+++ default.nix
@@ -1,7 +1,15 @@
-{ cabal, async, monadLoops, snap }:
-cabal.mkDerivation (self: {
+{ mkDerivation, async, base, containers, monad-loops
+, MonadCatchIO-transformers, snap, stdenv, stm, time, transformers
+}:
+mkDerivation {
   pname = "snap-error-collector";
-  version = "1.0.0";
+  version = "1.1.0";
   src = ./.;
-  buildDepends = [ async monadLoops snap ];
-})
+  buildDepends = [
+    async base containers monad-loops MonadCatchIO-transformers snap
+    stm time transformers
+  ];
+  homepage = "http://github.com/ocharles/snap-error-collector";
+  description = "Collect errors in batches and dispatch them";
+  license = stdenv.lib.licenses.bsd3;
+}
